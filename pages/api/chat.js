4edede70b47cc5b8c0243078e2789be4adb33a97
Wahab Shaikh <hey.wahabshaikh@gmail.com>
2023-04-26 17:23:43 +0530

    fix: file path

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -3,12 +3,14 @@ import { ConversationalRetrievalQAChain } from "langchain/chains";
 import { HNSWLib } from "langchain/vectorstores/hnswlib";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import * as fs from "fs";
+import path from "path";
+import { promises as fs } from "fs";
 
 export default async function (req, res) {
   const model = new OpenAI({});
 
-  const text = fs.readFileSync("data/sparkloop.txt", "utf8");
+  const dataDirectory = path.join(process.cwd(), "data");
+  const text = await fs.readFile(dataDirectory + "/sparkloop.txt", "utf8");
 
   const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000 });
   const docs = await textSplitter.createDocuments([text]);
